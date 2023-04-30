6a238b1937c42e24333c1594e7e74543f7482b4e
changliu <shunke.xu@cyg.com>
2023-04-30 18:18:40 +0800

    first commit

--- core-model/domain/src/main/java/org/xsk/domain/common/DomainPolicy.java
+++ core-model/domain/src/main/java/org/xsk/domain/common/DomainPolicy.java
@@ -6,7 +6,10 @@ public abstract class DomainPolicy {
         return SubscribePoint.BEFORE_DATA_COMMIT;
     }
 
+    //订阅触发节点
     public enum SubscribePoint {
-        BEFORE_DATA_COMMIT, AFTER_DATA_COMMIT, ASYNC
+        BEFORE_DATA_COMMIT,//数据事务提交前
+        AFTER_DATA_COMMIT,//数据事务提交后
+        ASYNC,//事件提交时异步执行
     }
 }
