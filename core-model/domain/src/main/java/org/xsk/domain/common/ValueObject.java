43ec47630d2e9773998766616bc93e42030a9f16
changliu <shunke.xu@cyg.com>
2023-06-21 11:54:40 +0800

    修缮ddd的基础包

--- core-model/domain/src/main/java/org/xsk/domain/common/ValueObject.java
+++ core-model/domain/src/main/java/org/xsk/domain/common/ValueObject.java
@@ -4,6 +4,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class ValueObject {
+    /**
+     * 若实体内值对象存储在不同的表,那么在对对象进行restore时可以值对象的表的id存入,方便后面进行实体更新的时候,可以用此找到值对象的表的id来进行更新操作
+     */
     Map<Object, Object> metaData = new ConcurrentHashMap<>();
 
     <T> T getMetaData(Object key, Class<T> valType) {
