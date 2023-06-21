b5725125a025a76fc0a6333dfaa1a8589edcb117
changliu <shunke.xu@cyg.com>
2023-06-21 23:41:40 +0800

    修缮ddd的基础包

--- core-model/domain/src/main/java/org/xsk/domain/common/EventBus.java
+++ core-model/domain/src/main/java/org/xsk/domain/common/EventBus.java
@@ -10,7 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 public class EventBus {
-    final static Map<DomainPolicy.SubscribePoint, Set<DomainPolicy<?>>> SUBSCRIBE_POINT_SET_CONCURRENT_HASH_MAP = new ConcurrentHashMap<>();
+    final static Map<DomainPolicy.SubscribePoint, Set<DomainPolicy<?>>> SUBSCRIBE_POINT_POLICY_MAP = new ConcurrentHashMap<>();
     final static ThreadLocal<Queue<DomainEvent>> BEFORE_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE = ThreadLocal.withInitial(ArrayDeque::new);
     final static ThreadLocal<Queue<DomainEvent>> AFTER_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE = ThreadLocal.withInitial(ArrayDeque::new);
 
@@ -18,36 +18,41 @@ public class EventBus {
         // : 2023/4/14 实现上根据订阅者DomainPolicy.subscribePoint()的值,
         //  决定事务前(false)投递给哪些订阅者,事务后(true)投递给哪些订阅者,还是马上异步执行
         @SuppressWarnings("unchecked")
-        Set<DomainPolicy<E>> asyncDomainPolicies = (Set) SUBSCRIBE_POINT_SET_CONCURRENT_HASH_MAP.get(DomainPolicy.SubscribePoint.ASYNC_IMMEDIATELY);
+        Set<DomainPolicy<E>> asyncDomainPolicies = (Set) SUBSCRIBE_POINT_POLICY_MAP.get(DomainPolicy.SubscribePoint.ASYNC_IMMEDIATELY);
         asyncDomainPolicies.parallelStream().forEach(eDomainPolicy -> executeIfNecessary(event, eDomainPolicy, false));
         @SuppressWarnings("unchecked")
-        Set<DomainPolicy<E>> sycDomainPolicies = (Set) SUBSCRIBE_POINT_SET_CONCURRENT_HASH_MAP.get(DomainPolicy.SubscribePoint.SYNC_IMMEDIATELY);
+        Set<DomainPolicy<E>> sycDomainPolicies = (Set) SUBSCRIBE_POINT_POLICY_MAP.get(DomainPolicy.SubscribePoint.SYNC_IMMEDIATELY);
         sycDomainPolicies.forEach(eDomainPolicy -> executeIfNecessary(event, eDomainPolicy, true));
         // : 2023/6/21 add event 2 BEFORE_EVENT_EMITTED,AFTER_EVENT_EMITTED policy wait list
         BEFORE_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE.get().add(event);
         AFTER_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE.get().add(event);
     }
 
-    public static void triggerBeforeMainProcessCompleted() {
-        triggerEventEmitted(BEFORE_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE, DomainPolicy.SubscribePoint.BEFORE_MAIN_PROCESS_COMPLETED);
+    static void triggerBeforeMainProcessCompleted() {
+        triggerMainProcessCompleted(BEFORE_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE, DomainPolicy.SubscribePoint.BEFORE_MAIN_PROCESS_COMPLETED);
     }
 
-    public static void triggerAfterMainProcessCompleted() {
-        triggerEventEmitted(AFTER_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE, DomainPolicy.SubscribePoint.AFTER_MAIN_PROCESS_COMPLETED);
+    static void triggerAfterMainProcessCompleted() {
+        triggerMainProcessCompleted(AFTER_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE, DomainPolicy.SubscribePoint.AFTER_MAIN_PROCESS_COMPLETED);
     }
 
-    private static void triggerEventEmitted(ThreadLocal<Queue<DomainEvent>> afterEventEmittedEventWaitingQueue, DomainPolicy.SubscribePoint afterEventEmitted) {
+    private static void triggerMainProcessCompleted(ThreadLocal<Queue<DomainEvent>> afterEventEmittedEventWaitingQueue, DomainPolicy.SubscribePoint afterEventEmitted) {
         try {
             Queue<DomainEvent> domainEvents = afterEventEmittedEventWaitingQueue.get();
             while (!domainEvents.isEmpty()) {
                 DomainEvent domainEvent = domainEvents.poll();
                 @SuppressWarnings("unchecked")
-                Set<DomainPolicy<DomainEvent>> domainPolicies = (Set) SUBSCRIBE_POINT_SET_CONCURRENT_HASH_MAP.get(afterEventEmitted);
+                Set<DomainPolicy<DomainEvent>> domainPolicies = (Set) SUBSCRIBE_POINT_POLICY_MAP.get(afterEventEmitted);
                 domainPolicies.forEach(domainPolicy -> executeIfNecessary(domainEvent, domainPolicy, true));
             }
         } finally {
             afterEventEmittedEventWaitingQueue.remove();
         }
+    }
+
+    static void cleanEventQueue(){
+        BEFORE_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE.remove();
+        AFTER_MAIN_PROCESS_COMPLETED_EVENT_WAITING_QUEUE.remove();
     }
 
     private static <E extends DomainEvent> void executeIfNecessary(E event, DomainPolicy<E> eDomainPolicy, boolean throwException) {
@@ -62,16 +67,16 @@ public class EventBus {
         }
     }
 
+    private static boolean isPolicySubscribeEvent(DomainPolicy<?> policy, DomainEvent event) {
+        return policy.eventClass().isInstance(event);
+    }
+
     static void registerPolicy(DomainPolicy<?> policy) {
-        Set<DomainPolicy<?>> domainPolicies = SUBSCRIBE_POINT_SET_CONCURRENT_HASH_MAP.computeIfAbsent(
+        Set<DomainPolicy<?>> domainPolicies = SUBSCRIBE_POINT_POLICY_MAP.computeIfAbsent(
                 policy.subscribePoint(),
                 k -> ConcurrentHashMap.newKeySet()
         );
         domainPolicies.add(policy);
-    }
-
-    static boolean isPolicySubscribeEvent(DomainPolicy<?> policy, DomainEvent event) {
-        return policy.eventClass().isInstance(event);
     }
 
 //    public static void main(String[] args) {
---
+++ core-model/domain/src/main/java/org/xsk/domain/common/SpringIntegration.java
@@ -0,0 +1,24 @@
+package org.xsk.domain.common;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronization;
+
+public class SpringIntegration {
+    @Component
+    public static class SpringTxEventInvoker implements TransactionSynchronization {
+
+        @Override
+        public void beforeCommit(boolean readOnly) {
+            EventBus.triggerBeforeMainProcessCompleted();
+        }
+
+        @Override
+        public void afterCompletion(int status) {
+            if (STATUS_COMMITTED != status) {
+                EventBus.cleanEventQueue();
+                return;
+            }
+            EventBus.triggerAfterMainProcessCompleted();
+        }
+    }
+}
