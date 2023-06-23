3a56740827dfde3d915a1baf8da30ab4a183c73c
changliu <shunke.xu@cyg.com>
2023-06-23 09:31:47 +0800

    修缮ddd的基础包

--- core-model/domain/src/main/java/org/xsk/domain/account/AccountFactory.java
+++ core-model/domain/src/main/java/org/xsk/domain/account/AccountFactory.java
@@ -2,11 +2,12 @@ package org.xsk.domain.account;
 
 import lombok.AllArgsConstructor;
 import org.xsk.domain.account.event.AccountCreated;
+import org.xsk.domain.common.DomainFactory;
 import org.xsk.domain.common.EventBus;
 
 
 @AllArgsConstructor
-public class AccountFactory {
+public class AccountFactory extends DomainFactory {
 
     public Account build(AccountStatus status, String name, String loginName, String password, Contact contact, PhysicalAddress address, AccountId parentAccountId) {
         Account account = new Account(status, name, loginName, password, contact, address, parentAccountId);
