066877a380cf8b78dd702c2942c1a1d8c2b6b215
changliu <shunke.xu@cyg.com>
2023-06-23 08:59:29 +0800

    修缮ddd的基础包

--- domain-common/src/main/java/org/xsk/domain/common/IllegalStateDomainException.java
+++ domain-common/src/main/java/org/xsk/domain/common/IllegalStateDomainException.java
@@ -1,7 +1,5 @@
 package org.xsk.domain.common;
 
-import lombok.NonNull;
-
 public class IllegalStateDomainException extends DomainException {
     protected IllegalStateDomainException(String message) {
         super(message);
