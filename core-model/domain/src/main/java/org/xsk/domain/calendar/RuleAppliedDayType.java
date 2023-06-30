ce931d8982917de9833c9d3ccbfca14f4ce66b3e
changliu <shunke.xu@cyg.com>
2023-06-30 22:25:18 +0800

    spring化

--- core-model/domain/src/main/java/org/xsk/domain/calendar/RuleAppliedDayType.java
+++ core-model/domain/src/main/java/org/xsk/domain/calendar/RuleAppliedDayType.java
@@ -3,7 +3,7 @@ package org.xsk.domain.calendar;
 public enum RuleAppliedDayType {
     NATURE_DAY(false), TO_WORK_DAY(false), TO_REST_DAY(false),
     ;
-    boolean applied;
+    final boolean applied;
 
     RuleAppliedDayType(boolean applied) {
         this.applied = applied;
