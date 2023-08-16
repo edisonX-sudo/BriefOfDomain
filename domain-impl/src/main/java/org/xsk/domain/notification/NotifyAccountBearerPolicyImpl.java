6f86fef8b149407184eb68e5f6cad1a6a29b72d9
changliu <shunke.xu@cyg.com>
2023-08-16 21:48:39 +0800

    spring化

--- domain-impl/src/main/java/org/xsk/domain/notification/NotifyAccountBearerPolicyImpl.java
+++ domain-impl/src/main/java/org/xsk/domain/notification/NotifyAccountBearerPolicyImpl.java
@@ -1,11 +1,14 @@
 package org.xsk.domain.notification;
 
+import org.springframework.stereotype.Component;
 import org.xsk.domain.account.Contact;
 import org.xsk.domain.notificaiton.NotifyAccountBearerPolicy;
 
+@Component
 public class NotifyAccountBearerPolicyImpl extends NotifyAccountBearerPolicy {
     @Override
     protected void notifyBearer(Contact contact) {
+        System.out.println("notify account bearer via contact");
         // TODO: 2023/4/14 notify account bearer via contact
     }
 }
