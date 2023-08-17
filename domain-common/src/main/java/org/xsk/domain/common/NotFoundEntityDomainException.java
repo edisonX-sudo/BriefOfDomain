e73a79c300d17b8c26ef9bb0406231170999321c
changliu <shunke.xu@cyg.com>
2023-08-17 15:16:00 +0800

    spring化

--- domain-common/src/main/java/org/xsk/domain/common/NotFoundEntityDomainException.java
+++ domain-common/src/main/java/org/xsk/domain/common/NotFoundEntityDomainException.java
@@ -1,15 +1,13 @@
 package org.xsk.domain.common;
 
 public abstract class NotFoundEntityDomainException extends DomainException {
-    private final Id<?> id;
 
     protected NotFoundEntityDomainException(Id<?> id, String message) {
         this(id, message, true);
     }
 
     protected NotFoundEntityDomainException(Id<?> id, String message, boolean msgWithId) {
-        super(msgWithId ? message + ": " + id.value() : message);
-        this.id = id;
+        super(msgWithId && id != null ? message + ": " + id.value() : message);
     }
 
 }
