5e17a4fd5e49f82b20f40fc37f1ce703460e8a90
changliu <shunke.xu@cyg.com>
2023-08-27 09:50:13 +0800

    spring化

--- domain-common/src/main/java/org/xsk/domain/common/ValueObject.java
+++ domain-common/src/main/java/org/xsk/domain/common/ValueObject.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
 
 public abstract class ValueObject extends AggregateComponent implements Serializable {
     protected static <T extends ValueObject> T cloneObject(T obj) {
-        return (T) ObjectUtil.clone(obj);
+        return ObjectUtil.clone(obj);
     }
 }
